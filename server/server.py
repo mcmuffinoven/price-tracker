5a3b618f970fec80c2797aed52eb230691ab2201
mcmuffinoven <ma.anthony1125@gmail.com>
2023-11-15 23:59:33 -0800

    Added new route to accept post request, and return the JSON

--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
 import json
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+import random
+from datetime import datetime
 
 # app instance
 app = Flask(__name__)
 CORS(app)
-
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/api/home", methods = ["GET"])
 def return_home():
@@ -18,6 +20,39 @@ def return_home():
     #     'people': ['Apple',"Banana","Cherry"]
     # })
 
+# New post request to add a new product to the database
+@app.route("/api/addProduct", methods = ["POST"])
+@cross_origin()
+def addProduct():
+    # 1. Add product to database 
+    # 2. If error in database, send back request with error 
+    # 3. Otherwise, return status SUCCESS
+    # handle the POST request
+    
+    prodID = random.randint(100, 1000)
+    prodStartPrice = random.randint(100,500)
+    prodCurPrice = random.randint(100,500)
+    prodLowestPrice = random.randint(100,500)
+    data = request.json
+    
+    productData = {
+        "id":prodID,
+        "productName": data["productName"],
+        "startingProductPrice": prodStartPrice,
+        "currentProductPrice": prodCurPrice,
+        "lowestProductPrice": prodLowestPrice,
+        "lowestProductPriceDate": datetime.now(),
+        "trackedSinceDate": datetime.now(),
+        "category": data["category"],
+        "saleBool": False
+    }
+    with open("products.json", 'r') as f:
+        data = json.load(f)
+    
+    sampleData={}
+    sampleData["Modules"] = 15
+    sampleData["Subject"] = "Maths"
+    return jsonify(productData)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
